b05d9a936a82b653cb8a7e2e8a524b8d50bdd7b6
Nikola Hristov <nikola@playform.cloud>
2022-08-06 11:24:06 +0300

    Initial commit

---
+++ tauri/src/fa.ts
@@ -0,0 +1,39 @@
+import Utils from "./utils";
+
+(async () => {
+	const refresh = async () => {
+		setInterval(async () => {
+			const focus = await Utils.getElement(".most-tracked-item");
+			const id = Math.floor(Math.random() * focus.length);
+
+			focus.item(id).click();
+
+			(
+				await Utils.getElement(
+					"#follow-aircraft,.setting.hide-aircraft,.ui-icon.ui-icon-closethick"
+				)
+			).forEach((el) => {
+				el.click();
+			});
+
+			(
+				await Utils.getElement(
+					".overlay-views-panel, .section search-overlay"
+				)
+			).forEach((el) => {
+				el.remove();
+			});
+
+			(
+				await Utils.getElement(".map-control-button.zoom-button.plus")
+			).forEach((el) => {
+				el.click();
+				el.click();
+				el.click();
+				el.click();
+			});
+		}, 13000);
+	};
+
+	await refresh();
+})();
---
+++ tauri/src/ta.ts
@@ -0,0 +1,70 @@
+import Utils from "./utils";
+
+(async () => {
+	const refresh = async () => {
+		// @ts-ignore
+		clearInterval(window.refreshInterval);
+
+		// @ts-ignore
+		window.refreshInterval = setInterval(async () => {
+			// @ts-ignore
+			clearInterval(window.refreshInterval);
+
+			(await Utils.getElement(".js-column")).forEach(async (el) => {
+				const focus = await Utils.getElement(".js-stream-item", el);
+				const length = focus.length;
+				const id = Math.floor(Math.random() * length);
+
+				if (
+					(
+						await Utils.getElement(
+							".btd-content-warning",
+							focus.item(id)
+						)
+					).length == 0
+				) {
+					(
+						await Utils.getElement(
+							".js-stream-item-content",
+							focus.item(id)
+						)
+					).forEach(async (el) => {
+						el.click();
+					});
+				}
+
+				setTimeout(async () => {
+					const focus = await Utils.getElement(".js-column-back");
+					const id = Math.floor(Math.random() * length);
+
+					focus.item(id).click();
+					await refresh();
+				}, 24000 + Math.floor(Math.random() * length * 1000));
+
+				setTimeout(async () => {
+					const focus = await Utils.getElement(".column-type-icon");
+					const id = Math.floor(Math.random() * length);
+
+					focus.item(id).click();
+					await refresh();
+				}, 24000 + Math.floor(Math.random() * length * 1000));
+			});
+		}, 13000);
+	};
+
+	setInterval(async () => {
+		(await Utils.getElement(".btd-clear-column-link")).forEach((el) => {
+			el.click();
+		});
+	}, 360000);
+
+	setInterval(async () => {
+		(await Utils.getElement(".js-translate-call-to-action")).forEach(
+			(el) => {
+				el.click();
+			}
+		);
+	}, 1000);
+
+	await refresh();
+})();
