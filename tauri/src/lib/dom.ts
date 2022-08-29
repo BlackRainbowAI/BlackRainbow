ca7353507cc230d732f4978513511d1b1e40efd2
Nikola Hristov <nikola@playform.cloud>
2022-08-29 19:34:09 +0300

    Cleanup

--- tauri/src/lib/dom.ts
+++ tauri/src/lib/dom.ts
@@ -1,7 +1,5 @@
 /**
- * "Get all elements that match the selector from the parent element."
- *
- * The function is async, so it returns a promise. The promise resolves to a NodeListOf<HTMLElement>
+ * GetElement returns a promise that resolves to a NodeListOf<HTMLElement>
  * @param {string} selector - The CSS selector to find the element.
  * @param {HTMLElement | Document} parent - HTMLElement | Document = document
  * @returns A promise that resolves to a NodeListOf<HTMLElement>
@@ -10,7 +8,7 @@ export const getElement = async (
 	selector: string,
 	parent: HTMLElement | Document = document
 ): Promise<NodeListOf<HTMLElement>> => {
-	return parent.querySelectorAll(selector) as NodeListOf<HTMLElement>;
+	return parent.querySelectorAll(selector);
 };
 
 export default { getElement };
