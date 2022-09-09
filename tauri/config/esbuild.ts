79b753f49b2bd6516ba5712b15002eee3177b17f
Nikola Hristov <nikola@lightrix.dev>
2022-09-09 13:43:01 +0300

    Cleanup

--- tauri/config/esbuild.ts
+++ tauri/config/esbuild.ts
@@ -3,29 +3,27 @@ import fs from "fs";
 
 const outDir = "dist";
 
-export default (): BuildOptions => {
-	return {
-		format: "iife",
-		minify: true,
-		outdir: outDir,
-		bundle: true,
-		platform: "browser",
-		target: ["edge104", "esnext"],
-		write: true,
-		sourcesContent: false,
-		plugins: [
-			{
-				name: "clean-dist",
-				setup(build: PluginBuild) {
-					build.onStart(async () => {
-						try {
-							await fs.promises.rm(outDir, {
-								recursive: true,
-							});
-						} catch (error) {}
-					});
-				},
+export default (): BuildOptions => ({
+	format: "iife",
+	minify: true,
+	outdir: outDir,
+	bundle: true,
+	platform: "browser",
+	target: ["edge104", "esnext"],
+	write: true,
+	sourcesContent: false,
+	plugins: [
+		{
+			name: "clean-dist",
+			setup(build: PluginBuild) {
+				build.onStart(async () => {
+					try {
+						await fs.promises.rm(outDir, {
+							recursive: true,
+						});
+					} catch (error) {}
+				});
 			},
-		],
-	};
-};
+		},
+	],
+});
