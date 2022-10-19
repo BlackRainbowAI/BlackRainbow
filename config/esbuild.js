0c4abd3dcf003c1e0b540fc9f7c04989f1d163e4
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-19 12:47:07 +0300

    Cleanup

--- config/esbuild.js
+++ config/esbuild.js
@@ -1,26 +1,26 @@
 import fs from "fs";
-
 const outDir = "dist";
 export default async () => ({
-	format: "esm",
-	minify: true,
-	outdir: outDir,
-	bundle: true,
-	platform: "browser",
-	target: ["edge104", "esnext"],
-	write: true,
-	plugins: [
-		{
-			name: "clean-dist",
-			setup(build) {
-				build.onStart(async () => {
-					try {
-						await fs.promises.rm(outDir, {
-							recursive: true,
-						});
-					} catch (error) {}
-				});
-			},
-		},
-	],
+    format: "esm",
+    minify: true,
+    outdir: outDir,
+    bundle: true,
+    platform: "browser",
+    target: ["edge104", "esnext"],
+    write: true,
+    plugins: [
+        {
+            name: "clean-dist",
+            setup(build) {
+                build.onStart(async () => {
+                    try {
+                        await fs.promises.rm(outDir, {
+                            recursive: true,
+                        });
+                    }
+                    catch (error) { }
+                });
+            },
+        },
+    ],
 });
