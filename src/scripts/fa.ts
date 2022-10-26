d24f93d9d13c53fe1749d8a92e356934b188233e
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-27 01:17:22 +0300

    squash!

---
+++ src/scripts/fa.ts
@@ -0,0 +1,43 @@
+import DOM from "../lib/dom.js";
+
+(async () => {
+	let interval: NodeJS.Timer;
+
+	const refresh = async () => {
+		clearInterval(interval);
+
+		interval = setInterval(async () => {
+			const focus = await DOM.getElement(".most-tracked-item");
+			const id = Math.floor(Math.random() * focus.length);
+
+			focus.item(id).click();
+
+			(
+				await DOM.getElement(
+					"#follow-aircraft,.setting.hide-aircraft,.ui-icon.ui-icon-closethick"
+				)
+			).forEach((el) => {
+				el.click();
+			});
+
+			(
+				await DOM.getElement(
+					".overlay-views-panel, .section search-overlay"
+				)
+			).forEach((el) => {
+				el.remove();
+			});
+
+			(
+				await DOM.getElement(".map-control-button.zoom-button.plus")
+			).forEach((el) => {
+				el.click();
+				el.click();
+				el.click();
+				el.click();
+			});
+		}, 13000);
+	};
+
+	await refresh();
+})();
