dc010f27c39cc3920886da7f78e40b9a7cf9737f
Nikola Hristov <nikola@nikolahristov.tech>
2023-01-20 16:20:04 +0200

    squash!

--- config/esbuild.js
+++ config/esbuild.js
@@ -1,25 +1,24 @@
 import * as fs from "fs";
 const outDir = "dist";
 export default {
-    format: "esm",
-    minify: true,
-    outdir: outDir,
-    bundle: true,
-    platform: "browser",
-    target: ["edge104", "esnext"],
-    plugins: [
-        {
-            name: "clean-dist",
-            setup(build) {
-                build.onStart(async () => {
-                    try {
-                        await fs.promises.rm(outDir, {
-                            recursive: true,
-                        });
-                    }
-                    catch (_error) { }
-                });
-            },
-        },
-    ],
+	format: "esm",
+	minify: true,
+	outdir: outDir,
+	bundle: true,
+	platform: "browser",
+	target: ["edge104", "esnext"],
+	plugins: [
+		{
+			name: "clean-dist",
+			setup(build) {
+				build.onStart(async () => {
+					try {
+						await fs.promises.rm(outDir, {
+							recursive: true,
+						});
+					} catch (_error) {}
+				});
+			},
+		},
+	],
 };
