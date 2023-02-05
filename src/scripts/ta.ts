2a70c77295105480beb5f7db4287928ac7bb7b4f
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-05 23:38:06 +0200

    Update cache action and add package.json and type checking

--- src/scripts/ta.ts
+++ src/scripts/ta.ts
@@ -9,58 +9,64 @@ import DOM from "../lib/dom.js";
 		interval = setInterval(async () => {
 			clearInterval(interval);
 
-			(await DOM.getElement(".js-column")).forEach(async (el) => {
-				const focus = await DOM.getElement(".js-stream-item", el);
-				const length = focus.length;
-				const id = Math.floor(Math.random() * length);
+			(await DOM.getElement(".js-column")).forEach(
+				async (el: HTMLElement) => {
+					const focus = await DOM.getElement(".js-stream-item", el);
+					const length = focus.length;
+					const id = Math.floor(Math.random() * length);
 
-				if (
-					(
-						await DOM.getElement(
-							".btd-content-warning",
-							focus.item(id)
-						)
-					).length === 0
-				) {
-					(
-						await DOM.getElement(
-							".js-stream-item-content",
-							focus.item(id)
-						)
-					).forEach(async (el) => {
-						el.click();
-					});
+					if (
+						(
+							await DOM.getElement(
+								".btd-content-warning",
+								focus.item(id)
+							)
+						).length === 0
+					) {
+						(
+							await DOM.getElement(
+								".js-stream-item-content",
+								focus.item(id)
+							)
+						).forEach(async (el: HTMLElement) => {
+							el.click();
+						});
+					}
+
+					setTimeout(async () => {
+						const focus = await DOM.getElement(".js-column-back");
+						const id = Math.floor(Math.random() * length);
+
+						focus.item(id).click();
+						await refresh();
+					}, 24000 + Math.floor(Math.random() * length * 1000.0));
+
+					setTimeout(async () => {
+						const focus = await DOM.getElement(".column-type-icon");
+						const id = Math.floor(Math.random() * length);
+
+						focus.item(id).click();
+						await refresh();
+					}, 24000 + Math.floor(Math.random() * length * 1000.0));
 				}
-
-				setTimeout(async () => {
-					const focus = await DOM.getElement(".js-column-back");
-					const id = Math.floor(Math.random() * length);
-
-					focus.item(id).click();
-					await refresh();
-				}, 24000 + Math.floor(Math.random() * length * 1000.0));
-
-				setTimeout(async () => {
-					const focus = await DOM.getElement(".column-type-icon");
-					const id = Math.floor(Math.random() * length);
-
-					focus.item(id).click();
-					await refresh();
-				}, 24000 + Math.floor(Math.random() * length * 1000.0));
-			});
+			);
 		}, 13000);
 	};
 
 	setInterval(async () => {
-		(await DOM.getElement(".btd-clear-column-link")).forEach((el) => {
-			el.click();
-		});
+		(await DOM.getElement(".btd-clear-column-link")).forEach(
+			(el: HTMLElement) => {
+				el.click();
+			}
+		);
 	}, 360000);
 
 	setInterval(async () => {
-		(await DOM.getElement(".js-translate-call-to-action")).forEach((el) => {
-			el.click();
-		});
+		(await DOM.getElement(".js-translate-call-to-action")).forEach(
+			(el: HTMLElement) => {
+				el.click();
+			}
+		);
 	}, 1000.0);
 
 	await refresh();
