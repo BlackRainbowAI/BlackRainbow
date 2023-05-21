5ea42190a1259ff86f7f2e743a0f39f56548bfaf
Nikola Hristov <nikola@nikolahristov.tech>
2023-05-21 22:38:37 +0300

    squash!

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,18 +7,15 @@ use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu
 
 fn main() {
 	tauri::Builder::default()
-    	
 		.system_tray(
 			SystemTray::new().with_menu(
 				SystemTrayMenu::new()
 					.add_item(CustomMenuItem::new("show".to_string(), "Show"))
 					.add_item(CustomMenuItem::new("hide".to_string(), "Hide"))
 					.add_item(CustomMenuItem::new("exit".to_string(), "Exit"))
-					.add_item(CustomMenuItem::new("run".to_string(), "Run Scripts"))
+					.add_item(CustomMenuItem::new("run".to_string(), "Run Scripts")),
 			),
 		)
-
-
 		.on_system_tray_event(|app: &tauri::AppHandle, event: SystemTrayEvent| {
 			if let SystemTrayEvent::MenuItemClick { id, .. } = event {
 				match id.as_str() {
@@ -28,8 +25,6 @@ fn main() {
 						});
 					}
 					"hide" => {
-
-
 						app.windows().into_iter().for_each(|(_label, window)| {
 							window.hide().unwrap();
 						});
@@ -37,27 +32,44 @@ fn main() {
 					"exit" => {
 						std::process::exit(0);
 					}
-					"run" => {
-						app.windows().into_iter().for_each(|(_label, window)| {
-					window.eval(&fs::read_to_string(window.app_handle().path_resolver()						.resolve_resource(format!("../dist/scripts/{}.js", window.label()))
-							.expect("Failed to resolve resource dir."),
-					)
-					.expect("Error while reading JS file."),
-				)
-				.expect("Script did not execute successfully.");
+					"run" => app.windows().into_iter().for_each(|(_label, window)| {
+						window
+							.eval(
+								&fs::read_to_string(
+									window
+										.app_handle()
+										.path_resolver()
+										.resolve_resource(format!(
+											"../dist/scripts/{}.js",
+											window.label()
+										))
+										.expect("Failed to resolve resource dir."),
+								)
+								.expect("Error while reading JS file."),
+							)
+							.expect("Script did not execute successfully.");
 
-				window.eval(&format!(
-					r#"var style = document.createElement('style'); style.innerHTML = `{}`; document.head.appendChild(style);"#,
-					&fs::read_to_string(
-						window.app_handle().path_resolver()
-							.resolve_resource(format!("../dist/styles/{}.css", window.label()))
-							.expect("Failed to resolve resource dir.")
-					)
-					.expect("Error while reading CSS file.")
-				))
-				.expect("Style did not load successfully.");
-						})
-					}
+						window
+							.eval(&format!(
+								r#"
+									var style = document.createElement('style');
+									style.innerHTML = `{}`;
+									document.head.appendChild(style);
+								"#,
+								&fs::read_to_string(
+									window
+										.app_handle()
+										.path_resolver()
+										.resolve_resource(format!(
+											"../dist/styles/{}.css",
+											window.label()
+										))
+										.expect("Failed to resolve resource dir.")
+								)
+								.expect("Error while reading CSS file.")
+							))
+							.expect("Style did not load successfully.");
+					}),
 					_ => {}
 				}
 			}
