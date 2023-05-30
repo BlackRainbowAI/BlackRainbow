231c53ac0062c9773475ba7d1620df055448282e
Nikola Hristov <nikola@nikolahristov.tech>
2023-05-30 03:59:07 +0300

    squash!

--- dist/lib/dom.d.ts
+++
@@ -1,9 +0,0 @@
-interface NodeListOf<TNode extends Node> extends NodeList {
-    item(index: number): TNode;
-    forEach(callbackfn: (value: TNode, key: number, parent: NodeListOf<TNode>) => void, thisArg?: any): void;
-    [index: number]: TNode;
-}
-declare const _default: {
-    getElement: (selector: string, parent?: HTMLElement | Document) => Promise<NodeListOf<HTMLElement>>;
-};
-export default _default;
