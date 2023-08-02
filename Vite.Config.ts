db3674cf822bbc622b91b2538b12f3f0e0acf0ef
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-03 01:26:01 +0300

    squash!

--- Vite.Config.ts
+++ Vite.Config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
 	envPrefix: ["VITE_", "TAURI_"],
 	build: {
 		target:
-			process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
-		minify: process.env.TAURI_DEBUG ? false : "esbuild",
-		sourcemap: !!process.env.TAURI_DEBUG,
+			process.env['TAURI_PLATFORM'] === "windows" ? "chrome105" : "safari13",
+		minify: process.env["TAURI_DEBUG"] ? false : "esbuild",
+		sourcemap: !!process.env["TAURI_DEBUG"],
 	},
 });
