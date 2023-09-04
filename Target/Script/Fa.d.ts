840540f0a6cfc7784fa761a064fedfa55665dfb1
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-04 19:04:29 +0300

    squash!

--- Target/Script/Fa.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" resolution-mode="require"/>
-declare global {
-    interface Window {
-        interval: NodeJS.Timer;
-        timeout: NodeJS.Timeout;
-    }
-}
-export {};
