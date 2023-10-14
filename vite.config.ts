4522db768cdca3edf84313015b4f5332f633b8ef
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 11:56:19 +0300

    squash!

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
+export default (await import("vite")).defineConfig({
 	publicDir: "./Public",
 	clearScreen: false,
 	server: {
