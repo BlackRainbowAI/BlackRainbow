7111679cc70d56dbc5e755b7912b3340f15985e4
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 16:49:49 +0300

    squash!

---
+++ Configuration/ESBuild.d.ts
@@ -0,0 +1,14 @@
+/**
+ * @module ESBuild
+ *
+ */
+declare const _default: {
+    format: "esm";
+    minify: true;
+    outdir: string;
+    bundle: true;
+    platform: "browser";
+    target: string[];
+    plugins: import("esbuild").Plugin[];
+};
+export default _default;
