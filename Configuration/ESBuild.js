753e42681cfff620422c49f40f57991cc0aaa29f
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-15 08:17:09 +0200

    squash!

--- Configuration/ESBuild.js
+++ Configuration/ESBuild.js
@@ -3,34 +3,36 @@
  *
  */
 export default {
-    format: "esm",
-    minify: true,
-    outdir: "Target",
-    bundle: true,
-    platform: "browser",
-    target: ["edge104", "esnext"],
-    plugins: [
-        {
-            name: "Target",
-            setup: ({ onStart, initialOptions: { outdir } }) => onStart(async () => {
-                try {
-                    outdir
-                        ? await (await import("fs/promises")).rm(outdir, {
-                            recursive: true,
-                        })
-                        : {};
-                }
-                catch (_Error) { }
-            }),
-        },
-        (await import("esbuild-plugin-copy")).copy({
-            resolveFrom: "out",
-            assets: [
-                {
-                    from: "./Source/styles/*.css",
-                    to: "./styles/",
-                },
-            ],
-        }),
-    ],
+	format: "esm",
+	minify: true,
+	outdir: "Target",
+	bundle: true,
+	platform: "browser",
+	target: ["edge104", "esnext"],
+	plugins: [
+		{
+			name: "Target",
+			setup: ({ onStart, initialOptions: { outdir } }) =>
+				onStart(async () => {
+					try {
+						outdir
+							? await (
+									await import("fs/promises")
+								).rm(outdir, {
+									recursive: true,
+								})
+							: {};
+					} catch (_Error) {}
+				}),
+		},
+		(await import("esbuild-plugin-copy")).copy({
+			resolveFrom: "out",
+			assets: [
+				{
+					from: "./Source/styles/*.css",
+					to: "./styles/",
+				},
+			],
+		}),
+	],
 };
