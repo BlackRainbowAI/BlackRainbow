e21bda1bd94a511fd610862ceff3b43fa54869ba
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-17 07:48:17 +0200

    squash!

--- Target/Library/DOM.d.ts
+++ Target/Library/DOM.d.ts
@@ -1,9 +1,19 @@
 interface NodeListOf<TNode extends Node> extends NodeList {
-    item(Index: number): TNode;
-    forEach(callbackfn: (value: TNode, key: number, parent: NodeListOf<TNode>) => void, thisArg?: any): void;
-    [index: number]: TNode;
+	item(Index: number): TNode;
+	forEach(
+		callbackfn: (
+			value: TNode,
+			key: number,
+			parent: NodeListOf<TNode>,
+		) => void,
+		thisArg?: any,
+	): void;
+	[index: number]: TNode;
 }
 declare const _default: {
-    getElement: (Selector: string, Parent?: HTMLElement | Document) => Promise<NodeListOf<HTMLElement>>;
+	getElement: (
+		Selector: string,
+		Parent?: HTMLElement | Document,
+	) => Promise<NodeListOf<HTMLElement>>;
 };
 export default _default;
