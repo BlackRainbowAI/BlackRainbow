0b20af8dcde4bdf5c4e357dad0c5f358c52206cd
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-27 03:07:50 +0200

    squash!

--- Configuration/ESBuild.d.ts
+++ Configuration/ESBuild.d.ts
@@ -3,12 +3,12 @@
  *
  */
 declare const _default: {
-    format: "esm";
-    minify: true;
-    outdir: string;
-    bundle: true;
-    platform: "browser";
-    target: string[];
-    plugins: import("esbuild").Plugin[];
+	format: "esm";
+	minify: true;
+	outdir: string;
+	bundle: true;
+	platform: "browser";
+	target: string[];
+	plugins: import("esbuild").Plugin[];
 };
 export default _default;
