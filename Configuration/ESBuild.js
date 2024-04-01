3ee563c6f9793dd8359bb9f0cf1c65d5ed9fbd00
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-01 17:34:28 +0300

    squash!

--- Configuration/ESBuild.js
+++ Configuration/ESBuild.js
@@ -3,36 +3,36 @@
  *
  */
 export default {
-	format: "esm",
-	minify: true,
-	outdir: "Target",
-	bundle: true,
-	platform: "browser",
-	target: ["edge104", "esnext"],
-	plugins: [
-		{
-			name: "Target",
-			setup: ({ onStart, initialOptions: { outdir } }) =>
-				onStart(async () => {
-					try {
-						outdir
-							? await (await import("fs/promises")).rm(outdir, {
-									recursive: true,
-							  })
-							: {};
-					} catch (_Error) {
-						console.log(_Error);
-					}
-				}),
-		},
-		(await import("esbuild-plugin-copy")).copy({
-			resolveFrom: "out",
-			assets: [
-				{
-					from: "./Source/styles/*.css",
-					to: "./styles/",
-				},
-			],
-		}),
-	],
+    format: "esm",
+    minify: true,
+    outdir: "Target",
+    bundle: true,
+    platform: "browser",
+    target: ["edge104", "esnext"],
+    plugins: [
+        {
+            name: "Target",
+            setup: ({ onStart, initialOptions: { outdir } }) => onStart(async () => {
+                try {
+                    outdir
+                        ? await (await import("fs/promises")).rm(outdir, {
+                            recursive: true,
+                        })
+                        : {};
+                }
+                catch (_Error) {
+                    console.log(_Error);
+                }
+            }),
+        },
+        (await import("esbuild-plugin-copy")).copy({
+            resolveFrom: "out",
+            assets: [
+                {
+                    from: "./Source/styles/*.css",
+                    to: "./styles/",
+                },
+            ],
+        }),
+    ],
 };
