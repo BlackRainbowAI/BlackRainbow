16086b09bd307552b758e3659def57d4dadedd5e
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-03 20:21:35 +0300

    squash!

--- Configuration/ESBuild.ts
+++ Configuration/ESBuild.ts
@@ -3,42 +3,8 @@
  *
  */
 export default {
-	color: true,
-	format: "esm",
-	metafile: true,
-	minify: true,
-	outdir: "Target",
-	platform: "browser",
 	bundle: true,
 	target: ["edge104", "esnext"],
-	write: true,
-	logLevel: "debug",
-	plugins: [
-		{
-			name: "Target",
-			setup: ({ onStart, initialOptions: { outdir } }) =>
-				onStart(async () => {
-					try {
-						outdir
-							? await (await import("fs/promises")).rm(outdir, {
-									recursive: true,
-							  })
-							: {};
-					} catch (_Error) {
-						console.log(_Error);
-					}
-				}),
-		},
-		(await import("@playform/copy")).default({
-			Resolve: "out",
-			Asset: [
-				{
-					from: "./Source/styles/*.css",
-					to: "./styles/",
-				},
-			],
-		}),
-	],
 } satisfies BuildOptions;
 
 import type { BuildOptions } from "esbuild";
