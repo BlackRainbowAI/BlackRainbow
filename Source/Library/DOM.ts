7b2e4aead7f8a40eef02468334fa3416565129e4
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-11 22:59:20 +0300

    squash!

---
+++ Source/Library/DOM.ts
@@ -0,0 +1,23 @@
+interface NodeListOf<TNode extends Node> extends NodeList {
+	item(Index: number): TNode;
+
+	forEach(
+		callbackfn: (
+			value: TNode,
+			key: number,
+			parent: NodeListOf<TNode>
+		) => void,
+		// biome-ignore lint/suspicious/noExplicitAny:
+		thisArg?: any
+	): void;
+
+	[index: number]: TNode;
+}
+
+export default {
+	getElement: async (
+		Selector: string,
+		Parent: HTMLElement | Document = document
+	): Promise<NodeListOf<HTMLElement>> =>
+		Parent.querySelectorAll<HTMLElement>(Selector),
+};
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+export default (await import("vite")).defineConfig({
+	publicDir: "./Public",
+	clearScreen: false,
+	server: {
+		port: 1420,
+		strictPort: true,
+		hmr: {
+			overlay: false,
+		},
+	},
+	envPrefix: ["VITE_", "TAURI_"],
+	build: {
+		outDir: "Target",
+		target: "chrome105",
+		minify: process.env["TAURI_DEBUG"] ? false : "esbuild",
+		sourcemap: !!process.env["TAURI_DEBUG"],
+	},
+});
